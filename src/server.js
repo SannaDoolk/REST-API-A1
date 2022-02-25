3acf4cdee009b3fd3a83f07b6ca0336b2faea199
Sanna Doolk <sd222si@student.lnu.se>
2022-02-25 14:11:56 +0100

    Removed unused code

--- src/server.js
+++ src/server.js
@@ -21,13 +21,9 @@ const main = async () => {
 
   app.use(helmet())
 
-  //app.use(cors())
-
   app.use(logger('dev'))
 
   app.use(express.json())
-
-  //app.use(express.json({ limit: '500kb' }))
 
   app.use('/', router)
 
